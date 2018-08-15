41eef33f6bca55c09eecc6acc68b7ff88bd70800
GnomeZworc <nicolas.boufidjeline@gmail.com>
2018-08-15 02:38:07 +0200

    Add token gestion for auth

--- app/api.py
+++ app/api.py
@@ -5,9 +5,28 @@
 
 from wsgiref.simple_server import make_server
 from pycnic.core import WSGI, Handler
+from pycnic.errors import HTTP_401
+from functools import wraps
 from variables import env
 
+def check_head_token(request):
+    token = request.get_header("token")
+    if token and token == env("TOKEN"):
+        return True
+    return False
+
+def require():
+    def wrapper(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            if check_head_token(args[0].request) == False:
+                raise HTTP_401("bad token")
+            return f(*args, **kwargs)
+        return wrapped
+    return wrapper
+
 class Home(Handler):
+    @require()
     def get(self):
         return {"message":"hello"}
 
