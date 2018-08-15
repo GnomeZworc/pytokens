1c3dd356491a2d2136908a1c2925f6d0712b7d7e
GnomeZworc <nicolas.boufidjeline@gmail.com>
2018-08-15 03:02:05 +0200

    Add test for token auth gestion

--- test/test_api.py
+++ test/test_api.py
@@ -2,13 +2,18 @@
 
 from requests import get
 
-base_url = "http://127.0.0.1:8080/"
+base_url = "http://127.0.0.1:8000/"
+api_token = 'api-token'
 
 class TestApiBase():
-    def get_response(self, url):
-        response = get(url)
+    def get_response(self, url, head={}):
+        response = get(url, headers=head)
         return response
-    def test_base_response_text(self):
-        assert self.get_response(base_url).text == '{"message": "hello"}'
-    def test_base_response(self):
-        assert self.get_response(base_url).status_code == 200
+    def test_base_response_with_token_text(self):
+        headers = {'token':api_token}
+        assert self.get_response(base_url, head=headers).text == '{"message": "hello"}'
+    def test_base_response_with_token(self):
+        headers = {'token':api_token}
+        assert self.get_response(base_url, head=headers).status_code == 200
+    def test_base_response_without_token(self):
+        assert self.get_response(base_url).status_code == 401
