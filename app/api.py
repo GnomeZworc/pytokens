1a9428d51c0398206bb5b64624e9d7512d3468a2
GnomeZworc <nicolas.boufidjeline@gmail.com>
2018-08-16 17:58:18 +0200

    Move mongo class in a new file

--- app/api.py
+++ app/api.py
@@ -3,35 +3,12 @@
 #
 #!/usr/bin/python3
 
-import datetime
-
 from wsgiref.simple_server import make_server
 from pycnic.core import WSGI, Handler
 from pycnic.errors import HTTP_401
 from functools import wraps
 from variables import env
-from pymongo import MongoClient
-
-
-class mongo():
-    connect = None
-    db = None
-    api_name = None
-
-    def __init__(self, host='127.0.0.1', port='80', database='database', protocol='mongodb', api=''):
-        self.api_name = api
-        self.connect = MongoClient(protocol + "://" + host + ":" + port + "/")
-        self.db = self.connect[database]
-        self.logs("api start")
-    def close(self):
-        self.connect.close()
-        self.logs("api stop")
-    def logs(self, message):
-        elem = {}
-        elem["timestamp"] = datetime.datetime.now()
-        elem["message"] = message
-        self.db[self.api_name + "_logs"].insert_one(elem)
-
+from database import mongo
 Database = None
 
 def check_head_token(request):
