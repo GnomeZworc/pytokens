e0db351eeca9701b9f22903989300aa0a4dadb58
GnomeZworc <nicolas.boufidjeline@gmail.com>
2018-09-11 17:13:27 +0200

    Add autodelete before check

--- app/require/__init__.py
+++ app/require/__init__.py
@@ -6,6 +6,7 @@
 from pycnic.errors import HTTP_401
 from functools import wraps
 from variables import env
+from time import time
 
 def check_head_token(request):
     token = request.get_header("token")
@@ -13,10 +14,17 @@ def check_head_token(request):
         return True
     return False
 
+def check_time(database):
+    ret = database.findMany('tokens', {})
+    for elem in ret:
+        if elem["limit_time"] < int(time()) and elem["limit_time"] != 0:
+            database.deleteOne('tokens', {"_id": elem["_id"]})
+
 def require(database):
     def wrapper(f):
         @wraps(f)
         def wrapped(*args, **kwargs):
+            check_time(database)
             database.logs("connect in " + args[0].request.path + " from " + args[0].request.ip)
             if check_head_token(args[0].request) == False:
                 database.logs("try to connect from " + args[0].request.ip + " with bad token")
